869cd3df550f8fd3825a644b4b22b270702465f3
UnhappyAlexMage <alexandr.bro03@gmail.com>
2026-07-24 04:48:12 +0500

    Delete old files

--- Unit-3/1/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function calculateDeposit($sum, $months, $percent) {
-        $profit = $sum * ($percent / 100) * ($months / 12);
-        return $sum + $profit;
-    };
-
-    echo calculateDeposit(36000, 6, 12);
-?>
